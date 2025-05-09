3bfa92e3263c5dbaa0a4b995e693806446d22f30
Dan Chuzhov <111654282+chuzhov@users.noreply.github.com>
2025-05-09 12:50:28 +0300

    split DEV and PROD modes for scheduler

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,15 +1,64 @@
 # core/scheduler.py
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+
+from core.config import DEBUG, TIMEZONE, WORKING_HOURS, DEBUG_SCHEDULER_INTERVAL
+
+from schemas.kobo_schema import convert_model_to_dict_list
 from services.kobo import fetch_int_list, get_int_duration
-from services.db_ops import insert_new_records
+from services.db_ops import insert_new_records, get_existing_uuids
 from services.logger import logger
 
 async def scheduled_job():
     logger.info("Scheduled job started.")
-    fetched = await fetch_int_list()
-    enriched = [await get_int_duration(r) for r in fetched]
-    await insert_new_records(enriched)
 
-scheduler = AsyncIOScheduler(timezone="local")
-scheduler.add_job(scheduled_job, CronTrigger(hour="7-22", minute=0))
+    saved_uuids = await get_existing_uuids()
+    if saved_uuids:
+        logger.info(f"Fetched {len(saved_uuids)} existing UUIDs from the database.")
+
+    fetched = await fetch_int_list()
+    if not fetched:
+        logger.critical("No records to process.")
+        return
+#    else:
+#        logger.info(f"Fetched {len(fetched)} records from the external API.")
+    
+    if saved_uuids:
+        fetched = [r for r in fetched if r.uuid not in saved_uuids]
+    logger.info(f"New records to process: {len(fetched)}")
+
+    # TODO DEBUG!
+    fetched = fetched[:10] if DEBUG else fetched     # Limit to 10 records for testing
+
+    for record in fetched:
+        if record.audit_URL:
+            interview_duration = await get_int_duration(record.audit_URL)
+            record.interview_duration = interview_duration if interview_duration is not None else None
+        
+    await insert_new_records(
+        submissions = convert_model_to_dict_list( fetched )
+    )
+
+scheduler = AsyncIOScheduler(timezone = TIMEZONE)
+def setup_jobs():
+    if DEBUG:
+        scheduler.add_job(
+            scheduled_job, 
+            IntervalTrigger(
+                minutes=DEBUG_SCHEDULER_INTERVAL
+            ),  # Run every 5 minutes
+            id="debug_job",  # Unique ID for the job
+            replace_existing = False  # Actually, this is the default behavior
+        )
+        logger.info("Scheduler is running in DEBUG mode. Job is set to run every 5 minutes.")
+    else:
+        scheduler.add_job(
+            scheduled_job,
+            CronTrigger(
+                hour = f"{WORKING_HOURS["start"]}-{WORKING_HOURS["end"]}", 
+                minute = 0,
+            )  
+        )
+        logger.info(f"Scheduler is running in PRODUCTION mode. Job is set to run every hour from {WORKING_HOURS['start']} to {WORKING_HOURS['end']}.")
+    
