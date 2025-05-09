69976bf799acc3b0a8a619a996a34d506fe9235d
Dan Chuzhov <111654282+chuzhov@users.noreply.github.com>
2025-05-09 14:06:12 +0300

    update Interview model

--- models/interviews.py
+++ models/interviews.py
@@ -5,6 +5,6 @@ from core.database import Base
 class Interview(Base):
     __tablename__ = "intervies"
     uuid = Column(String, primary_key=True, index=True)
-    enumerator_id = Column(String)
-    audit_url = Column(String, nullable=True)
-    duration = Column(Float, nullable=True)
+    enumerator_Id = Column(String)
+    audit_URL = Column(String, nullable=True)
+    interview_duration = Column(Float, nullable=True)
