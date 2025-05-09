56a20a74935ef1d34db2da5a37bdcb71263fbff0
Dan Chuzhov <111654282+chuzhov@users.noreply.github.com>
2025-05-09 17:59:53 +0300

    add /interviews/count route

--- routes/interviews.py
+++ routes/interviews.py
@@ -1,9 +1,19 @@
 # api/endpoints.py
 from fastapi import APIRouter
-from services.db_ops import get_all_records
+from services.db_ops import get_all_records, get_records_count
 
 router = APIRouter()
 
 @router.get("/interviews")
 async def read_records():
+    """Fetch all interview records."""
     return await get_all_records()
+
+
+@router.get("/interviews/count", status_code=200)
+async def status():
+    """Return the status of the server."""
+    int_count = await get_records_count()
+    return {
+        "interviews_count": int_count
+    }
