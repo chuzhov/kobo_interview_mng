3fc8c28cb99a3880f766c858845c9363c6509227
Dan Chuzhov <111654282+chuzhov@users.noreply.github.com>
2025-05-09 12:49:24 +0300

    add DB init log message

--- core/database.py
+++ core/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+
 from .config import DATABASE_URL
+from services.logger import logger
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
 
 async def init_db():
-    async with engine.begin() as conn:
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+            logger.info("Database tables created successfully.")
+    except Exception as e:
+        # Use run_sync to execute the synchronous create_all method in an async context
         await conn.run_sync(Base.metadata.create_all)
+        logger.critical(f"An error occurred during database initialization: {e}")
+
