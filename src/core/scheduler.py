ad9a98fbd021947a5b86801b648020d452f58343
Dan Chuzhov <111654282+chuzhov@users.noreply.github.com>
2025-05-15 14:38:33 +0300

    refactor: update scheduled job to include fetch submissions and improve structure

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -4,20 +4,25 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
 
 from core.config import DEBUG, DEBUG_SCHEDULER_INTERVAL, TIMEZONE, WORKING_HOURS
-from schemas.kobo_schema import convert_model_to_dict_list
+import schemas.kobo_schema as schemas
 from services.db_ops import get_existing_uuids, insert_new_records
-from services.kobo import fetch_int_list, get_int_duration
+from services.kobo import fetch_kobo_data, get_int_duration, fetch_submissions
 from services.logger import logger
 
 
-async def scheduled_job():
+async def scheduled_job__Get_interview_duration():
     logger.info("Scheduled job started.")
+
+    await fetch_submissions()
 
     saved_uuids = await get_existing_uuids()
     if saved_uuids:
         logger.info(f"Fetched {len(saved_uuids)} existing UUIDs from the database.")
 
-    fetched = await fetch_int_list()
+    fetched = await fetch_kobo_data(
+        schema=schemas.FormSubmissionInterview,
+        fields=['metadata/enumerator_Id', '_attachments']
+    )
     if not fetched:
         logger.critical("No records to process.")
         return
@@ -37,17 +42,17 @@ async def scheduled_job():
                 interview_duration if interview_duration is not None else None
             )
 
-    await insert_new_records(submissions=convert_model_to_dict_list(fetched))
+    await insert_new_records(submissions=schemas.convert_model_to_dict_list(fetched))
 
 
-scheduler = AsyncIOScheduler(timezone=TIMEZONE)
+scheduler = AsyncIOScheduler()
 
 
 def setup_jobs():
     if DEBUG:
         scheduler.add_job(
-            scheduled_job,
-            IntervalTrigger(minutes=DEBUG_SCHEDULER_INTERVAL),  # Run every 5 minutes
+            func=scheduled_job__Get_interview_duration,
+            trigger=IntervalTrigger(minutes=DEBUG_SCHEDULER_INTERVAL),  # Run every 5 minutes
             id="debug_job",  # Unique ID for the job
             replace_existing=False,  # Actually, this is the default behavior
         )
@@ -56,11 +61,14 @@ def setup_jobs():
         )
     else:
         scheduler.add_job(
-            scheduled_job,
-            CronTrigger(
+            func=scheduled_job__Get_interview_duration,
+            trigger=CronTrigger(
+                timezone=TIMEZONE,
                 hour=f'{WORKING_HOURS["start"]}-{WORKING_HOURS["end"]}',
                 minute=0,
             ),
+            id="production_job",  # Unique ID for the job
+            replace_existing=False,  # Actually, this is the default behavior
         )
         logger.info(
             f"Scheduler is running in PRODUCTION mode. Job is set to run every hour from {WORKING_HOURS['start']} to {WORKING_HOURS['end']}."
